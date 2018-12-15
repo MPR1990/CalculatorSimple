529fe6f7ce65ba28f0e80b1bda0aad5b09365b71
Marcos Palavecino Rivero <mpalavecinorivero@hotmail.com>
2018-12-14 22:27:48 -0300

    Main program

--- CalculatorSimple/CalculatorSimple.cpp
+++ CalculatorSimple/CalculatorSimple.cpp
@@ -4,18 +4,49 @@
 #include "pch.h"
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	char op;
+	float num1, num2;
+
+	cout << "Enter operator either + or - or * or /: ";
+	cin >> op;
+
+	cout << "Enter first operand: " << flush;
+	cin >> num1;
+
+	cout << "Enter second operand: " << flush;
+	cin >> num2;
+
+	switch (op)
+	{
+	case '+':
+		cout << num1 + num2;
+		break;
+
+	case '-':
+		cout << num1 - num2;
+		break;
+
+	case '*':
+		cout << num1 * num2;
+		break;
+
+	case '/':
+		cout << num1 / num2;
+		break;
+
+	default:
+		// If the operator is other than +, -, * or /, error message is shown
+		cout << "Error! Selected operation is not correct";
+		break;
+	}
+
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
